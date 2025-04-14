5aa3f0d085dd85414d2cb0ab9d8cde51a3034343
YuvaRaj Vodiboina <yuvarajv952@gmail.com>
2025-04-14 16:46:58 +0530

    adding keys

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, device/xiaomi/pipa/device.mk)
 
 # Inherit keys
-# $(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
 
 # Device config
 TARGET_ENABLE_BLUR := true
